08edd17301f54c2dc6e96465d8b76d4274e1bb28
Favio10 <favio_olivera@hotmail.com>
2025-02-04 23:33:54 -0300

    primero

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const withSession = () => {
 
     client.on('auth_failure', () => {
         spinner.stop();
-        console.log('Error de autenticacion, vuelve a generar el QRCODE');
+        console.log('Error de autenticacion, vuelve a generar el QRCODE para luego');
     })
     
     client.initialize();
