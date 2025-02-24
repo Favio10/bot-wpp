ae60e8a21766585e9883794472f69301535c9c8e
Favio10 <favio_olivera@hotmail.com>
2025-02-23 23:55:11 -0300

    primer commit

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const withSession = () => {
 
 const withOutSession = () => {
 
-    console.log("No existe session activa de wpp, verificar conexion d");
+    console.log("No existe session activa de wpp, verificar conexion de");
     client = new Client();
     client.on('qr', qr => {
         qrcode.generate(qr, {small: true});
